8c3d80df5c8174dd8024e29339daf727c842a5cb
rose-line <1886792+rose-line@users.noreply.github.com>
2023-10-17 09:15:17 +0200

    Ajout de Hello world

--- src/main/java/fr/pgah/App.java
+++ src/main/java/fr/pgah/App.java
@@ -1,8 +1,9 @@
 package fr.pgah;
 
 public class App {
-    
-    public static void main(String[] args) {
-    }
-    
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
 }
